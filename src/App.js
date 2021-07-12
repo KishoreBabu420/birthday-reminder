fd62c393044bd69fc842b23620b6b540dfae134b
Kishore <kishore.sai37@gmail.com>
2021-07-12 21:34:56 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  const clearHandler = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className='container'>
+        <List people={people} />
+        <button onClick={clearHandler}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
